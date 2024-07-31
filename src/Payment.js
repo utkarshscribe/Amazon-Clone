883bc2785965796064702d5e4f72bafa2492a715
utkarshpandeypandey25@gmail.com <utkarshpandeypandey25@gmail.com>
2024-07-31 11:07:44 +0530

    <<<

--- src/Payment.js
+++ src/Payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Payment.css'
 import { useStateValue } from './Stateprovider'
 import CheckoutProduct from './CheckoutProduct';
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
 import { getBasketTotal } from './reducer';
+import axios from 'axios';
 
 function Payment() {
 
@@ -14,11 +15,37 @@ function Payment() {
     const stripe = useStripe();
     const Elements = useElements();
 
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
-    const [disable, setDisabled] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
 
-    const handleSubmit = e =>{
+    useEffect(() => {
+        //generate the special stripe secret which allows us to 
+        // charge a customer
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: 'post',
+                // stripe expects the total in a currencies submits
+                url:'/payments/create?total=${getBasketTotal(basket) * 100}'
+            });
+            setClientSecret(response.data.clientSecret)
+        }
+
+        getClientSecret();
+    }, [basket] )
+
+    const handleSubmit = async(event) =>{
         // do all the fancy stripe stuff....
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: Elements.getElement(CardElement)
+            }
+        })
     }
 
     const handleChange = event =>{
@@ -80,12 +107,10 @@ function Payment() {
                           <CurrencyFormat 
                             renderText={(value) => (
                                 <>
-                                <p>
-                                    Subtotal ({basket.length} items): <strong>{` ${value} `}</strong>
-                                </p>
-                                <small className='subtotal-gift'>
-                                    <input type='checkbox'/> This order contains gifts
-                                </small>
+                                <h3>
+                                   Order total ({basket.length} items): <strong>{` ${value} `}</strong>
+                                </h3>
+                                
                                 </>
                             )}
                             decimalScale={2}
@@ -94,7 +119,13 @@ function Payment() {
                             thousandSeparator={true}
                             prefix={'$'}
                           />
+                          <button disabled={processing || disabled || succeeded}>
+                            <span>{ processing ? <p>Processing</p> : "Buy Now"}</span>
+                          </button>
                         </div>
+
+                        {/* Errors */}
+                        {error && <div>{error}</div>}
                     </form>
                 </div>
             </div>
